ab0227e4541700af5580042b8d79988b54673876
Tylenardo <tlparsons00@gmail.com>
2019-10-13 11:32:45 -0700

    Questing: quest object and related scripts

---
+++ Assets/Scripts/Quests/Quest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Quest", menuName = "Make New Quest", order = 0)]
+public class Quest : ScriptableObject
+{
+    public bool isQuestActive;
+    public bool isQuestComplete = false;
+
+    public string questName;
+    public string questDescription;
+    public QuestGoals goals;
+    public float experienceReward;
+    public float goldReward;
+    public GameObject itemReward;
+
+}
---
+++ Assets/Scripts/Quests/QuestGoals.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class QuestGoals
+{
+    public GoalType goalType;
+
+    public int requiredAmount;
+    public int currentAmount;
+    public string goalName;
+
+    public bool IsReached()
+    {
+        return (currentAmount >= requiredAmount);
+    }
+}
+
+public enum GoalType
+{
+    Kill,
+    Collect
+
+}
